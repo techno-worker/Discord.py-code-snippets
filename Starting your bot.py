f20afbe66c0f3c55a21516140e73e27297cdce04
Gamerz <77479931+techno-worker@users.noreply.github.com>
2023-03-17 15:06:13 +0530

    Update Starting your bot.py

--- Starting your bot.py
+++ Starting your bot.py
@@ -4,8 +4,13 @@ from discord.ext import commands
 
 client=commands.Bot(command_prefix=['your_prefix'],intents=discord.Intents.all())
 
+
 #All your other codes will be here
 
+
+@client.event
+async def on_ready():
+	print(f"Connect To Bot: {client.user.name}")
 
 client.run("token_here")
 
