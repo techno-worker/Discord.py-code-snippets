e2736b0e59bce089bd3d5791d2e03ebb555ed133
Gamerz <77479931+techno-worker@users.noreply.github.com>
2023-03-17 15:22:41 +0530

    Update changing bot presence.py

--- changing bot presence.py
+++ changing bot presence.py
@@ -2,11 +2,11 @@ import discord
 from discord.ext import commands
 
 TOKEN="token_here"
-client=commands.Bot(command_prefix=['>'],intents=discord.Intents.all())
+client=commands.Bot(command_prefix=['your_prefix'],intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-  	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name="To >help"))
+  	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening,name="to your commands"))
   
 
 client.run(TOKEN)
